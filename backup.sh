912d3606b8ce80b986e6e1d3a6961189b7bbedf9
上野山真史 <n267@uenoyamamasashinoiMac.local>
2019-07-01 10:51:22 +0900

    swift ring create

--- backup.sh
+++ backup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-nowdt=`date '+%Y%m%d'`
+cd /home/ec2-user/glico/walter-compose-glico
 
+nowdt=`date '+%Y%m%d'`
+deldt=`date '+%Y%m%d' --date "3 days ago"`
+
+echo ${nowdt}
 sudo elasticdump  --input=http://localhost:9200/ --output=$  | gzip > ./elastic/backup/backup.json.gz
 sudo docker-compose exec mongo mongodump --port 27017 --out /data/backup
 
 sudo tar czf  storagebk${nowdt}.tgz ./elastic/backup ./mongo/backup ./swift
 
 aws s3 mv storagebk${nowdt}.tgz s3://clouds-work/backup/
+aws s3 rm s3://clouds-work/backup/storagebk${deldt}.tgz
